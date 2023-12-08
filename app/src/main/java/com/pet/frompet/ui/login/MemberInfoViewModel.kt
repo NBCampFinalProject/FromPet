1fef93ea28b1d370e9207f907d6cb0ef000b7912
4seokhyeon <tktjrgus@naver.com>
2023-12-08 14:30:43 +0900

    [리펙토링] 회원정보저장 파이어베이스 리포지토리 패턴적용 ViewModel

--- app/src/main/java/com/pet/frompet/ui/login/MemberInfoViewModel.kt
+++ app/src/main/java/com/pet/frompet/ui/login/MemberInfoViewModel.kt
@@ -6,7 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import com.pet.frompet.data.model.CommunityHomeData
+import com.pet.frompet.data.model.User
 import com.pet.frompet.data.repository.memberinfo.MemberInfoRepository
 import com.pet.frompet.data.repository.memberinfo.MemberInfoRepositoryImp
 import kotlinx.coroutines.launch
@@ -19,27 +22,70 @@ class MemberInfoViewModel(
     val petSpinnerType: LiveData<List<CommunityHomeData>> = _petSpinnerType
 
     private val _selectPetType = MutableLiveData<CommunityHomeData>()
-    val selectPetType:LiveData<CommunityHomeData> = _selectPetType
+    val selectPetType: LiveData<CommunityHomeData> = _selectPetType
+
+    private val _petProfileLiveData = MutableLiveData<String>()
+    val petProfileLiveData: LiveData<String> = _petProfileLiveData
+
+    private val _saveFireStore: MutableLiveData<Result<Unit>> = MutableLiveData()
+    val saveFireStore: LiveData<Result<Unit>> = _saveFireStore
 
 
     fun getMemberInfoSpinner() {
         viewModelScope.launch {
+            try{
             val communityHomeData = memberInfoRepository.getMemberSpinner()
             _petSpinnerType.postValue(communityHomeData)
+        }catch (_: Exception){
+
+        }
+        }
+    }
+    fun saveToFireStore(user: User) {
+        viewModelScope.launch {
+            try {
+                val result = memberInfoRepository.saveToFireStore(user)
+                _saveFireStore.postValue(result)
+            } catch (e: Exception) {
+                _saveFireStore.postValue(Result.failure(e))
+            }
+        }
+    }
+    fun contentUpload(petProfile: String?) {
+        viewModelScope.launch {
+            try {
+                if (petProfile == null) {
+                    throw IllegalArgumentException("petProfile is null")
+                }
+
+                val imageUrl = memberInfoRepository.uploadPetProfile(petProfile)
+                showToast("이미지 업로드 성공")
+
+                // Set the value of the LiveData
+                _petProfileLiveData.value = imageUrl
+            } catch (e: Exception) {
+                showToast("이미지 업로드 실패: ${e.message}")
+            }
         }
     }
 
-    fun setSelectType(item: CommunityHomeData){
-        _selectPetType.value = item
+    fun updatePetProfileLiveData(imageUrl: String) {
+        _petProfileLiveData.value = imageUrl
+    }
+
+    private fun showToast(message: String) {
+        /*  Toast.makeText(this, message, Toast.LENGTH_SHORT).show()*/
     }
 }
 
 class MemberInfoViewModelFactory(
-    private val context: Context
+    private val context: Context,
+    private val storage: FirebaseStorage,
+    private val fireStore: FirebaseFirestore
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MemberInfoViewModel::class.java)) {
-            return MemberInfoViewModel(MemberInfoRepositoryImp(context)) as T
+            return MemberInfoViewModel(MemberInfoRepositoryImp(context, storage,fireStore)) as T
         } else {
             throw IllegalArgumentException("Not found ViewModel class.")
         }
